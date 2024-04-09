41a7a365cff58b1bc184437838b1caf965a98750
FluffyTG <salvadoorc1@gmail.com>
2024-04-09 04:17:04 -0700

    Item Added

--- src/main/java/fluff/fluffsstuff/FluffsStuff.java
+++ src/main/java/fluff/fluffsstuff/FluffsStuff.java
@@ -1,5 +1,6 @@
 package fluff.fluffsstuff;
 
+import fluff.fluffsstuff.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -12,6 +13,6 @@ public class FluffsStuff implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
