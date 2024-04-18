323a5460b1ec8c5c7350915914145a4333e08704
FluffyTG <salvadoorc1@gmail.com>
2024-04-18 02:53:48 -0700

    Changed RNG for Tomatoes

--- src/main/java/fluff/fluffsstuff/block/custom/TomatoCropBlock.java
+++ src/main/java/fluff/fluffsstuff/block/custom/TomatoCropBlock.java
@@ -6,10 +6,24 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.CropBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.ItemStack;
+import net.minecraft.loot.LootTable;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.IntProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
+
+import java.util.Random;
 
 public class TomatoCropBlock extends CropBlock
 {
@@ -39,5 +53,22 @@ public class TomatoCropBlock extends CropBlock
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder)
     {
         builder.add(AGE);
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (this.getAge(state) == 5) {
+            Random tomatoRNG = new Random();
+            Random tomatoSeedRNG = new Random();
+            int tomatoCount = 1+tomatoRNG.nextInt(2);
+            int tomatoSeedCount = tomatoSeedRNG.nextInt(2);
+
+            player.giveItemStack(new ItemStack(ModItems.TOMATO, tomatoCount));
+            player.giveItemStack(new ItemStack(ModItems.TOMATO_SEEDS, tomatoSeedCount));
+            player.playSound(SoundEvents.ENTITY_ITEM_PICKUP, SoundCategory.NEUTRAL, 0.5f, 3f);
+            world.setBlockState(pos, state.with(AGE, 4), Block.NOTIFY_ALL);
+        }
+
+        return ActionResult.success(world.isClient);
     }
 }
